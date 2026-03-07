0600da5270568d7662a49b9194443d17f83a19bf
HuynhPTa <Huynhpete0205@gmail.com>
2026-03-07 16:43:45 +0700

    Add files via upload

---
+++ fitness-be/app/Models/Notification.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'sender_id',
+        'type',
+        'title',
+        'message',
+        'data',
+        'is_read'
+    ];
+
+    protected $casts = [
+        'data' => 'array'
+    ];
+}
