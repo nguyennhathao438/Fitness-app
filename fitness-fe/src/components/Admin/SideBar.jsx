199ba743e037f9f27214ec4dd199160231b2d08f
Hao Nguyen <hao362k5@gmail.com>
2026-03-15 16:04:02 +0700

    setup module authority

--- fitness-fe/src/components/Admin/SideBar.jsx
+++ fitness-fe/src/components/Admin/SideBar.jsx
@@ -9,8 +9,12 @@ import {
   TextAlignJustifyIcon,
 } from "lucide-react";
 import { NavLink } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 export default function SideBar({ collapsed, setCollapsed }) {
+  const permissions = useSelector((state) => state.auth.permissions);
+  const hasPermission = (code) => {
+    return permissions?.includes(code);
+  };
   return (
     <div
       className={`fixed top-0 left-0 h-full bg-gradient-to-r from-purple-600 to-indigo-600 text-white transition-all duration-300 ${collapsed ? "w-24" : "w-64"}`}
@@ -52,49 +56,55 @@ export default function SideBar({ collapsed, setCollapsed }) {
               Dashboard
             </span>
           </NavLink>
-          <NavLink
-            to="/admin/user"
-            className={({ isActive }) =>
-              `flex items-center px-4 py-2 rounded-md cursor-pointer transition
+          {hasPermission("user.read") && (
+            <NavLink
+              to="/admin/user"
+              className={({ isActive }) =>
+                `flex items-center px-4 py-2 rounded-md cursor-pointer transition
                             hover:bg-purple-500 hover:translate-x-1
                             ${isActive ? "bg-purple-600" : ""}`
-            }
-          >
-            <CircleUserRoundIcon className="mr-2 size-5" />
-            {!collapsed && <span className="text-lg">Users</span>}
-          </NavLink>
-          <NavLink
-            to="/admin/packages"
-            className={({ isActive }) =>
-              `flex items-center px-4 py-2 rounded-md cursor-pointer transition
-                            hover:bg-purple-500 hover:translate-x-1
-                            ${isActive ? "bg-purple-600" : ""}`
-            }
-          >
-            <PackageIcon className="text-white inline-block mr-2 size-5"></PackageIcon>
-            <span
-              className={`${
-                collapsed ? "hidden" : "inline"
-              } max-sm:hidden text-lg`}
+              }
             >
-              Packages
-            </span>
-          </NavLink>
-          <NavLink
-            to="/admin/message"
-            className={({ isActive }) =>
-              `flex items-center px-4 py-2 rounded-md cursor-pointer transition
+              <CircleUserRoundIcon className="mr-2 size-5" />
+              {!collapsed && <span className="text-lg">Users</span>}
+            </NavLink>
+          )}
+          {hasPermission("package.read") && (
+            <NavLink
+              to="/admin/packages"
+              className={({ isActive }) =>
+                `flex items-center px-4 py-2 rounded-md cursor-pointer transition
                             hover:bg-purple-500 hover:translate-x-1
                             ${isActive ? "bg-purple-600" : ""}`
-            }
-          >
-            <MessageCircleIcon className=" text-white inline-block mr-2 size-5"></MessageCircleIcon>
-            <span
-              className={`${collapsed ? "hidden" : "inline"} max-sm:hidden text-lg`}
+              }
             >
-              Messages
-            </span>
-          </NavLink>
+              <PackageIcon className="text-white inline-block mr-2 size-5"></PackageIcon>
+              <span
+                className={`${
+                  collapsed ? "hidden" : "inline"
+                } max-sm:hidden text-lg`}
+              >
+                Packages
+              </span>
+            </NavLink>
+          )}
+          {hasPermission("message_admin.read") && (
+            <NavLink
+              to="/admin/message"
+              className={({ isActive }) =>
+                `flex items-center px-4 py-2 rounded-md cursor-pointer transition
+                            hover:bg-purple-500 hover:translate-x-1
+                            ${isActive ? "bg-purple-600" : ""}`
+              }
+            >
+              <MessageCircleIcon className=" text-white inline-block mr-2 size-5"></MessageCircleIcon>
+              <span
+                className={`${collapsed ? "hidden" : "inline"} max-sm:hidden text-lg`}
+              >
+                Messages
+              </span>
+            </NavLink>
+          )}
           <NavLink
             to="/admin/message-pt"
             className={({ isActive }) =>
@@ -110,36 +120,40 @@ export default function SideBar({ collapsed, setCollapsed }) {
               Messages PT
             </span>
           </NavLink>
-          <NavLink
-            to="/admin/order"
-            className={({ isActive }) =>
-              `flex items-center px-4 py-2 rounded-md cursor-pointer transition
+          {hasPermission("invoice.read") && (
+            <NavLink
+              to="/admin/order"
+              className={({ isActive }) =>
+                `flex items-center px-4 py-2 rounded-md cursor-pointer transition
                             hover:bg-purple-500 hover:translate-x-1
                             ${isActive ? "bg-purple-600" : ""}`
-            }
-          >
-            <ShoppingCartIcon className=" text-white inline-block mr-2 size-5"></ShoppingCartIcon>
-            <span
-              className={`${collapsed ? "hidden" : "inline"} max-sm:hidden text-lg`}
+              }
             >
-              Orders
-            </span>
-          </NavLink>
-          <NavLink
-            to="/admin/role"
-            className={({ isActive }) =>
-              `flex items-center px-4 py-2 rounded-md cursor-pointer transition
+              <ShoppingCartIcon className=" text-white inline-block mr-2 size-5"></ShoppingCartIcon>
+              <span
+                className={`${collapsed ? "hidden" : "inline"} max-sm:hidden text-lg`}
+              >
+                Orders
+              </span>
+            </NavLink>
+          )}
+          {hasPermission("permission.read") && (
+            <NavLink
+              to="/admin/role"
+              className={({ isActive }) =>
+                `flex items-center px-4 py-2 rounded-md cursor-pointer transition
                                   hover:bg-purple-500 hover:translate-x-1
                                   ${isActive ? "bg-purple-600" : ""}`
-            }
-          >
-            <ShieldCheckIcon className=" text-white inline-block mr-2 size-5"></ShieldCheckIcon>
-            <span
-              className={`${collapsed ? "hidden" : "inline"} max-sm:hidden text-lg`}
+              }
             >
-              RoleSet
-            </span>
-          </NavLink>
+              <ShieldCheckIcon className=" text-white inline-block mr-2 size-5"></ShieldCheckIcon>
+              <span
+                className={`${collapsed ? "hidden" : "inline"} max-sm:hidden text-lg`}
+              >
+                RoleSet
+              </span>
+            </NavLink>
+          )}
           <NavLink
             to="/admin/exercise"
             className={({ isActive }) =>
