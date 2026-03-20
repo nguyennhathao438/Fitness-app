e1937770ffc6a565b689a139ac4f338026e8e1b7
AnhHUY <hoanganhhuy200499@gmail.com>
2026-03-20 09:37:56 +0700

    update

--- fitness-fe/src/components/member/Profile/modals/EditProfile.jsx
+++ fitness-fe/src/components/member/Profile/modals/EditProfile.jsx
@@ -82,6 +82,7 @@ export default function EditProfileModal({ open, onClose, member }) {
         <Modal
             title={"Chỉnh sửa thông tin"}
             bgColor={"bg-gray-900"}
+            border={"border-yellow-400 border-2 border"}
             txtColor={"text-white"}
             open={open}
             onClose={onClose}>
@@ -126,10 +127,10 @@ export default function EditProfileModal({ open, onClose, member }) {
                 </div>
 
                 <div className="flex justify-center gap-4 mt-2">
-                    <button type="button" className="px-4 py-1 bg-gray-300 rounded-md hover:bg-gray-400" onClick={() => { onClose();handleResetForm();}}>
+                    <button type="button" className="px-6 py-1 bg-gray-300 rounded-md hover:shadow-lg hover:shadow-gray-400/30 hover:bg-gray-400" onClick={() => { onClose();handleResetForm();}}>
                         Hủy
                     </button>
-                    <button type="submit" className="px-4 py-2 bg-purple-500 text-white rounded-md hover:bg-purple-600">
+                    <button type="submit" className="px-6 py-2 bg-yellow-300 text-purple-900 hover:shadow-lg hover:shadow-yellow-400/30 rounded-md hover:scale-110">
                         {isLoading ? "Đang lưu ..." : "Lưu"}
                     </button>
                 </div>
